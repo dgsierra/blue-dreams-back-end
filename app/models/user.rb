44ac56f954597b699fdbe27d5ba8faf606f87139
robinbenitezmora <robinbenitez1990@gmail.com>
2023-01-19 21:02:53 -0500

    Add code lines in the models in order to prevent N + 1 problem

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
 
-  has_many :reservations
+  has_many :reservations, counter_cache: true
 
   validates :name, presence: true
   validates :email, presence: true
