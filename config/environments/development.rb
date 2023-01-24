d68a0a9e94a0519e31bcbd3d49fe8369d6fe8bbd
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-01-24 11:46:56 -0600

    Fix error in heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,9 +19,6 @@ Rails.application.configure do
     Bullet.rollbar = true
     Bullet.add_footer = true
     Bullet.skip_html_injection = false
-    Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
-    Bullet.stacktrace_excludes = [ 'their_gem', 'their_middleware', ['my_file.rb', 'my_method'], ['my_file.rb', 16..20] ]
-    Bullet.slack = { webhook_url: 'http://some.slack.url', channel: '#default', username: 'notifier' }
   end
   # Settings specified here will take precedence over those in config/application.rb.
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_021846) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_21_023041) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_021846) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.boolean "admin", default: false
-    t.boolean "license"
+    t.boolean "license", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
